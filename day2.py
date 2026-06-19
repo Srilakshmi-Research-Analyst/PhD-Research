34495950151db0ae04b5469daf3acf3be40ea546
Srilakshmi-Research-Analyst <usrilakshmi92@gmail.com>
2026-06-19 17:07:05 +0530

    Day2: Variables and User Input

---
+++ day2.py
@@ -0,0 +1,14 @@
+# Day 2 - variables and user Input
+print("Welcome to Day2 python practice!")
+name = input("Enter Your name: ")
+age = int(input("Enter your age: "))
+qualification = input("Enter your qualification: ")
+
+print("\n--------Student Details---------")
+print("Name:", name)
+print("Age:", age)
+print("Qualification:", qualification)
+
+next_year_age = age + 1
+
+print("Next year your age will be:", next_year_age)
